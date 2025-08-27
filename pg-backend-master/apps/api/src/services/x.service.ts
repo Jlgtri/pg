380b5bd03a3f192bd7b054e5f14058ad74703bf6
Tymur <bagdtim1049@gmail.com>
2025-08-27 15:07:30 +0300

    Update x.service.ts

--- pg-backend-master/apps/api/src/services/x.service.ts
+++ pg-backend-master/apps/api/src/services/x.service.ts
@@ -24,7 +24,7 @@ export class XService {
       );
     }
     if (!res.data) {
-      throw new UnprocessableEntityException('Tweet not found');
+      throw new UnprocessableEntityException('Follow not found');
     }
   }
 
